bb9252357a417a3d79007f6902f8d01c81693a5a
SaulinhoDevs <165098314+saulobmelo@users.noreply.github.com>
2025-07-27 17:40:53 -0300

    update demais classes dos objetos

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,4 +1,8 @@
+import manager.SensorManager;
+
 public class Main {
     public static void main(String[] args) {
+        SensorManager manager = new SensorManager();
+        manager.exibirDadosSensores();
     }
 }
---
+++ src/src/display/DisplayConsole.java
@@ -0,0 +1,7 @@
+package display;
+
+public class DisplayConsole {
+    public void mostrarDados(String tipo, String valor) {
+        System.out.println("[" + tipo + "]: " + valor);
+    }
+}
